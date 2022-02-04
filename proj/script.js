c96bdfb24731b57a573d5d54f81ffc8241ed4070
DeWurgerZax <91907750+DeWurgerZax@users.noreply.github.com>
2022-02-04 13:46:58 +0200

    added function remover
    remover delete worker in fixated condition. Check comments

--- proj/script.js
+++ proj/script.js
@@ -9,35 +9,48 @@ const workers = ['Name: Margara, salary: 59€, pets: cat hamster','Name: Zuhra,
 'Name: Samon Dalas, salary: 270₽, pets: parrot dog'];
 
 class Worker { // объект работника
-    constructor(name, salary, pets){
-        this.name = name;
-        this.salary = salary;
-        this.pets = pets;
-    }
+  constructor(name, salary, pets){
+      this.name = name;
+      this.salary = salary;
+      this.pets = pets;
+  }
 }
 
 function createWorker(name, salary, pets){ // фабрика работников
-    return new Worker(name, salary, pets);
+  return new Worker(name, salary, pets);
 };
 
+const arrOfObjWorkers = []; // массив из объектов работников
+
 const splitter = (arr) => { // разделитель массива и формирование объектов, которые в дальнейшем становятся работниками
-    let reg = /(.*:\s)(.*)/; // во 2-ой группе будет значение ключа объекта
-    let NewArr = [];
-    for(let i=0; i < arr.length; i += 1){
-        let person = arr[i].split(', ');
-        person[0] = String(person[0]).replace(reg, '$2'); // поле name: содержит содержимое 2-ой группы. Например Margara
-        person[1] = String(person[1]).replace(reg, '$2'); // аналогично с salary
-        person[2] = String(person[2]).replace(reg, '$2'); // pets
-        NewArr.push(createWorker(person[0], person[1], person[2]));
-        checkPets(person[2]); // проверка работников на наличие питомцев 
-    }
-    return NewArr;
+  let reg = /(.*:\s)(.*)/; // во 2-ой группе будет значение ключа объекта
+  let NewArr = arrOfObjWorkers;
+  for(let i=0; i < arr.length; i += 1){
+      let person = arr[i].split(', ');
+      person[0] = String(person[0]).replace(reg, '$2'); // поле name: содержит содержимое 2-ой группы. Например Margara
+      person[1] = String(person[1]).replace(reg, '$2'); // аналогично с salary
+      person[2] = String(person[2]).replace(reg, '$2'); // pets
+      NewArr.push(createWorker(person[0], person[1], person[2]));
+      //checkPets(person[2]); // проверка работников на наличие питомцев 
+  }
+  return NewArr;
 }
 
-const checkPets = (str) => {
-    let petReg = /h..ster/gi; // если у работника есть hamster - зафиксировать 
-    str.match(petReg) !== null ? console.log(str) : console.log('not hamsters');
+// const checkPets = (str) => {
+//   let petReg = /h..ster/gi; // если у работника есть hamster - зафиксировать 
+//   str.match(petReg) !== null ? console.log(str) : console.log('not hamsters');
+// }
+
+const remover = (array) => {
+	let petReg = /h..ster/gi; // если у работника есть hamster - зафиксировать 
+	const cloneArray = array.slice(0); // клон массива, но в этом будут удалены те, у кого есть hamster
+	for(let i = 0; i < array.length; i += 1){
+		cloneArray[i].pets.match(petReg) !== null ? cloneArray[i] = null : '';
+	}
+	return cloneArray;
 }
 
 const array = splitter(workers); // массив из объектов работников 
 console.log(array);
+const arrayWoutPets = remover(arrOfObjWorkers);
+console.log(arrayWoutPets);
