9fcb92e02f04dda60ff1c5ca820d028ec97bb990
Alex <85021541+Alexininn@users.noreply.github.com>
2022-02-06 00:12:09 +0300

    Delete proj directory

--- proj/scripts/globalVars.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const arrOfObjWorkers = []; // массив из объектов работников
-
-let allow = false;
-const remBut = document.getElementById('remBut');
-const confBut = document.getElementById('confBut');
